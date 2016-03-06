06c3f36ea6e125564dc8f27c56d073aefd721b65
Sim Ee Shan <eeshansim@gmail.com>
2016-03-06 12:52:49 -0800

    basic landing page view layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
   end
 
   def edit
   end
 
+  private
   # whitelist parameters
   def user_params
-    params.require(:user).permit(:id, :email, :password, :name, :slug, :surname, :given_name, :image_url, :mobile_number, 
-      :gender, :birthday, :active, :default_brand_id, :default_zone_id, :default_postal_code, :current_cart_token, :timezone, role_ids: [])
+    params.require(:user).permit(:id, :email, :password, :name, :first_name, :last_name, :image_url, :mobile_number,
+                                 :active, :timezone, :login_count, :last_login_at, role_ids: [])
   end
 end
