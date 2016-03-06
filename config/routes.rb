8e616627410640e0ee8dc3f43f5afc281621190c
Sim Ee Shan <eeshansim@gmail.com>
2016-03-05 23:03:00 -0800

    user object

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'main#index'
+
+  resources :users do 
+    get 'users' => 'users#index'
+    get 'users/:id' => 'users#show'
+    get 'users/new'
+    get 'users/edit'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
